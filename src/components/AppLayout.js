3ff888ccbbfda7a28554179388566e7742fb5071
Pranav Singhal <pranavsinghal96@gmail.com>
2019-09-21 22:14:54 +0530

    first commit

---
+++ src/components/AppLayout.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import {Breadcrumb, Icon, Layout, Menu} from "antd";
+
+const AppLayout = ({children}) => {
+    const { Header, Content, Footer, Sider } = Layout;
+    return (
+        <Layout style={{minHeight: '100vh'}}>
+            <Header className={'header'}>
+                <div className={'logo'}> GenoBank</div>
+            </Header>
+
+
+            <Content style={{padding: '0 50px'}}>
+                <Breadcrumb style={{ margin: '16px 0' }}>
+                    <Breadcrumb.Item>Home</Breadcrumb.Item>
+                    <Breadcrumb.Item>Dashboard</Breadcrumb.Item>
+                </Breadcrumb>
+                <Layout className={'layout-content'}>
+                    <Sider
+                        collapsible
+                        breakpoint="lg"
+                    >
+                        <Menu
+                            mode="inline"
+                            defaultOpenKeys={['sub1']}
+                        >
+                            <Menu.SubMenu
+                                key="sub1"
+                                title={
+                                    <span>
+                  <Icon type="user" />
+                  User Actions
+                </span>
+                                }
+                            >
+                                <Menu.Item key="1">Action 1</Menu.Item>
+                                <Menu.Item key="2">Action 2</Menu.Item>
+                                <Menu.Item key="3">Action 3</Menu.Item>
+                                <Menu.Item key="4">Action 4</Menu.Item>
+                            </Menu.SubMenu>
+                        </Menu>
+                    </Sider>
+
+
+                    <Content style={{ padding: '0 24px', minHeight: 280 }}>{children}</Content>
+
+                </Layout>
+            </Content>
+
+        </Layout>
+    );
+};
+
+export default AppLayout;
---
+++ src/components/Dashboard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import 'antd/dist/antd.css';
+import { Layout, Menu, Breadcrumb, Icon } from 'antd';
+import AppLayout from "./AppLayout";
+const Dashboard = () => {
+
+
+    return (
+        <AppLayout>
+            content
+        </AppLayout>
+
+    );
+};
+
+export default Dashboard;
